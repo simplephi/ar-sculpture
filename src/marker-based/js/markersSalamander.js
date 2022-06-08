0a2d7369c1b48ab5a0c66a0ffdb02c5b32975ba5
simplephi <cleverdarkness@gmail.com>
2022-06-08 15:29:18 +0800

    test combine

--- src/marker-based/js/markersSalamander.js
+++ src/marker-based/js/markersSalamander.js
@@ -79,7 +79,71 @@ AFRAME.registerComponent('markers_start',{
                   cameraGPS.setAttribute('rotation-reader', '');
                   sceneEl.appendChild(cameraGPS);
 
+                  //////////////////////////////////Detect marker found and lost////////////////////////////////////////////////////
+                  AFRAME.registerComponent('registerevents', {
+                     init: function () {
+                       const marker = this.el;
 
+                       marker.addEventListener("markerFound", ()=> {
+
+
+                         console.log('Marker Found');
+
+
+                         // var cameraPosition = document.querySelector('[gps-camera]').object3D.position;
+                         // console.log('Marker Position: ', cameraPosition);
+
+
+                         // ADD MODELS AFTER MARKER FOUND
+                         navigator.geolocation.getCurrentPosition(function(position) {
+
+                             const newCoordinates = JSON.stringify({
+                               lat: position.coords.latitude,
+                               lng: position.coords.longitude
+                             });
+                             // console.log('stringified coordinates', newCoordinates);
+                             // localStorage.setItem('coords', newCoordinates);
+
+
+                              // CREATE
+                             var body = document.querySelector('a-scene');
+
+                             var salamander = document.createElement('a-entity');
+
+                             // spiral.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+
+
+
+                              salamander.setAttribute('gltf-model', '#salamander');
+                              salamander.setAttribute('animation-mixer', '');
+
+
+                              salamander.object3D.position.set(0, 0, 0);
+                              salamander.object3D.scale.set(50, 50, 50);
+                              salamander.object3D.rotation.set(0, 0, 0);
+
+
+                              salamander.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+
+                             body.appendChild(salamander);
+
+
+
+                             console.log('Successfully show AR on marker location based once');
+
+                           }, error, optionsNewcoord);
+
+
+
+                       }, {once : true});
+
+                       marker.addEventListener("markerLost",() =>{
+
+                         console.log('Marker Lost');
+                       });
+                     },
+                   });
+                  ////////////////////////////////////////////////////////////////////////////////////
 
 
               }
@@ -88,69 +152,3 @@ AFRAME.registerComponent('markers_start',{
 	}
 
 });
-
-//////////////////////////////////Detect marker found and lost////////////////////////////////////////////////////
-AFRAME.registerComponent('registerevents', {
-   init: function () {
-     const marker = this.el;
-
-     marker.addEventListener("markerFound", ()=> {
-
-
-       console.log('Marker Found');
-
-
-       // var cameraPosition = document.querySelector('[gps-camera]').object3D.position;
-       // console.log('Marker Position: ', cameraPosition);
-
-
-       // ADD MODELS AFTER MARKER FOUND
-       navigator.geolocation.getCurrentPosition(function(position) {
-
-           const newCoordinates = JSON.stringify({
-             lat: position.coords.latitude,
-             lng: position.coords.longitude
-           });
-           // console.log('stringified coordinates', newCoordinates);
-           // localStorage.setItem('coords', newCoordinates);
-
-
-            // CREATE
-           var body = document.querySelector('a-scene');
-
-           var salamander = document.createElement('a-entity');
-
-           // spiral.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
-
-
-
-            salamander.setAttribute('gltf-model', '#salamander');
-            salamander.setAttribute('animation-mixer', '');
-
-
-            salamander.object3D.position.set(0, 0, 0);
-            salamander.object3D.scale.set(50, 50, 50);
-            salamander.object3D.rotation.set(0, 0, 0);
-
-
-            salamander.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
-
-           body.appendChild(salamander);
-
-
-
-           console.log('Successfully show AR on marker location based once');
-
-         }, error, optionsNewcoord);
-
-
-
-     }, {once : true});
-
-     marker.addEventListener("markerLost",() =>{
-
-       console.log('Marker Lost');
-     });
-   },
- });
-////////////////////////////////////////////////////////////////////////////////////
