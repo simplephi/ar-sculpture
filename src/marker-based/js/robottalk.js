247b15aa306b6751257ebe747489cc192390e8de
simplephi <cleverdarkness@gmail.com>
2022-06-23 11:44:01 +0800

    add sound and marsrobot

--- src/marker-based/js/robottalk.js
+++ src/marker-based/js/robottalk.js
@@ -169,3 +169,21 @@ AFRAME.registerComponent('markers_start',{
 	}
 
 });
+
+//Detect marker found and lost
+AFRAME.registerComponent('registerevents', {
+		init: function () {
+			const marker = this.el;
+
+			marker.addEventListener("markerFound", ()=> {
+				var markerId = marker.id;
+        console.log('Marker found: ', markerId);
+
+			});
+
+			marker.addEventListener("markerLost",() =>{
+				var markerId = marker.id;
+				console.log('Marker Lost: ', markerId);
+			});
+		},
+	});
