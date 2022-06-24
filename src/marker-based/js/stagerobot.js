6f97ed97928c5dee0a8230080fb60006541a9e82
simplephi <cleverdarkness@gmail.com>
2022-06-24 12:52:38 +0800

    add new stagerobot

--- src/marker-based/js/stagerobot.js
+++ src/marker-based/js/stagerobot.js
@@ -58,7 +58,7 @@ AFRAME.registerComponent('markers_start',{
                     marsrobotMarker.setAttribute('id', 'marsrobotMarker');
 
                     marsrobotMarker.object3D.position.set(0, 0, 0);
-                    marsrobotMarker.object3D.scale.set(3, 3, 3);
+                    marsrobotMarker.object3D.scale.set(1, 1, 1);
                     marsrobotMarker.object3D.rotation.set(-90, -90, 0);
 
                     // marsrobotMarker.setAttribute('sound', {
@@ -81,7 +81,7 @@ AFRAME.registerComponent('markers_start',{
                     marsrobotMarker.setAttribute('id', 'marsrobotMarker');
 
                     marsrobotMarker.object3D.position.set(0, 0, 0);
-                    marsrobotMarker.object3D.scale.set(3, 3, 3);
+                    marsrobotMarker.object3D.scale.set(1, 1, 1);
                     marsrobotMarker.object3D.rotation.set(-90, -90, 0);
 
                     // marsrobotMarker.setAttribute('sound', {
